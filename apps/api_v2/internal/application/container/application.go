13d2dd99b0121d36b3d82e055179fce1f8d2dfc0
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-03-20 12:24:29 -0500

    ADDED: implemented ports in application container, ready for dependency injection

--- apps/api_v2/internal/application/container/application.go
+++ apps/api_v2/internal/application/container/application.go
@@ -1,4 +1,48 @@
 package container
 
+import (
+	"fmt"
+
+	user_usecases "../usecases/user"
+)
+
+
+
+// UserUseCases holds all user-related use cases available in the application.
+type UserUseCases struct {
+	Register     *user_usecases.RegisterUseCase
+	Login        *user_usecases.LoginUseCase
+	GetData      *user_usecases.GetDataUseCase
+	RefreshToken *user_usecases.RefreshTokenUseCase
+}
+
 type Application struct {
+	Dependencies ApplicationDependencies
+	UserModule   UserUseCases
+}
+
+func NewApplication(deps ApplicationDependencies) (*Application, error) {
+
+	if err := deps.CheckDependencies(); err != nil {
+		return nil, fmt.Errorf("application dependencies check failed: %w", err)
+	}
+	
+	app := &Application{Dependencies: deps}
+	app.UserModule = UserUseCases{
+		Register: user_usecases.NewRegisterUseCase(
+			deps.RegisterService,
+			deps.UserService,
+			deps.PasswordHasher,
+		),
+		Login: user_usecases.NewLoginUseCase(
+			deps.LoginService,
+			deps.PasswordHasher,
+		),
+		GetData: user_usecases.NewGetDataUseCase(deps.LoginService),
+		RefreshToken: user_usecases.NewRefreshTokenUseCase(
+			deps.TokenService,
+		),
+	}
+
+	return app, nil
 }
---
+++ apps/api_v2/internal/application/container/dependencies.go
@@ -0,0 +1,58 @@
+package container
+
+import (
+	"fmt"
+
+	ports "../ports/user"
+)
+
+
+// ApplicationDependencies groups all contract-based dependencies required by
+// application use cases.
+type ApplicationDependencies struct {
+	RegisterService ports.RegisterPort
+	LoginService    ports.LoginPort
+	UserService     ports.UserServicePort
+	TokenService    ports.TokenServicePort
+	PasswordHasher  ports.PasswordHasherPort
+}
+
+func NewApplicationDependencies(
+	registerService ports.RegisterPort,
+	loginService ports.LoginPort,
+	userService ports.UserServicePort,
+	tokenService ports.TokenServicePort,
+	passwordHasher ports.PasswordHasherPort,
+) ApplicationDependencies {
+	return ApplicationDependencies{
+		RegisterService: registerService,
+		LoginService:    loginService,
+		UserService:     userService,
+		TokenService:    tokenService,
+		PasswordHasher:  passwordHasher,
+	}
+}
+
+func (deps ApplicationDependencies) CheckDependencies() error {
+	if deps.RegisterService == nil {
+		return fmt.Errorf("RegisterService dependency is not provided")
+	}
+
+	if deps.LoginService == nil {
+		return fmt.Errorf("LoginService dependency is not provided")
+	}
+
+	if deps.UserService == nil {
+		return fmt.Errorf("UserService dependency is not provided")
+	}
+
+	if deps.TokenService == nil {
+		return fmt.Errorf("TokenService dependency is not provided")
+	}
+
+	if deps.PasswordHasher == nil {
+		return fmt.Errorf("PasswordHasher dependency is not provided")
+	}
+	
+	return  nil
+}

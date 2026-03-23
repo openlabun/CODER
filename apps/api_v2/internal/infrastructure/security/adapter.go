ce64e47890417cae43705ca83f2bcd7657ec2e87
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-03-22 22:21:16 -0500

    FIXED: new logic for encrypting passwords. Now it depends into a deterministic system

--- apps/api_v2/internal/infrastructure/security/adapter.go
+++ apps/api_v2/internal/infrastructure/security/adapter.go
@@ -1,31 +1,44 @@
 package security
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/hex"
+	"os"
+)
+
+var requiredSymbols = []byte("!@#$_-")
 
 type SecurityAdapter struct {
+	pepper string
 }
 
 func NewSecurityAdapter() *SecurityAdapter {
-	return &SecurityAdapter{}
+	return &SecurityAdapter{
+		pepper: os.Getenv("PASSWORD_HASH_PEPPER"),
+	}
 }
 
 func (a *SecurityAdapter) Hash(plain string) (string, error) {
-	password, err := bcrypt.GenerateFromPassword([]byte(plain), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
+	// Deterministic hash for integrations that compare exact values.
+	sum := sha256.Sum256([]byte(a.pepper + ":" + plain))
+
+	base := hex.EncodeToString(sum[:])
+	prefix := []byte{
+		byte('A' + (sum[0] % 26)),
+		byte('a' + (sum[1] % 26)),
+		byte('0' + (sum[2] % 10)),
+		requiredSymbols[sum[3]%byte(len(requiredSymbols))],
 	}
 
-	return string(password), nil
+	return string(prefix) + base, nil
 }
 
 func (a *SecurityAdapter) Compare(hash, plain string) (bool, error) {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plain))
-	if err == nil {
-		return true, nil
-	}
-	if err == bcrypt.ErrMismatchedHashAndPassword {
-		return false, nil
+	candidate, err := a.Hash(plain)
+	if err != nil {
+		return false, err
 	}
 
-	return false, err
+	return subtle.ConstantTimeCompare([]byte(hash), []byte(candidate)) == 1, nil
 }

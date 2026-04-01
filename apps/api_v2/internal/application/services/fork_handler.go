3833da2c02dc7455f7c1cc9069fe4b43adfc0a0e
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-03-31 22:38:02 -0500

    REFACTOR: implemented IOVariableRepository in fork_handler services

--- apps/api_v2/internal/application/services/fork_handler.go
+++ apps/api_v2/internal/application/services/fork_handler.go
@@ -5,75 +5,73 @@ import (
 	"fmt"
 
 	Entities "github.com/openlabun/CODER/apps/api_v2/internal/domain/entities/exam"
-	domain_services "github.com/openlabun/CODER/apps/api_v2/internal/domain/services"
 	examRepository "github.com/openlabun/CODER/apps/api_v2/internal/domain/repositories/exam"
+	domain_services "github.com/openlabun/CODER/apps/api_v2/internal/domain/services"
 
 	factory "github.com/openlabun/CODER/apps/api_v2/internal/domain/factory/exam"
-
-
 )
 
-func ForkChallenge (ctx context.Context, 
-	challenge Entities.Challenge, 
-	userID string, 
+func ForkChallenge(ctx context.Context,
+	challenge Entities.Challenge,
+	userID string,
 	challengeRepository examRepository.ChallengeRepository,
 	testCaseRepository examRepository.TestCaseRepository,
 	ioVariableRepository examRepository.IOVariableRepository,
-	) (*Entities.Challenge, error) {
+) (*Entities.Challenge, error) {
 
-		// [STEP 1] Get Challenge TestCases
-		testCases, err := testCaseRepository.GetTestCasesByChallengeID(ctx, challenge.ID)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get test cases for challenge: %w", err)
-		}
-
-		// [STEP 2] Fork IO Variables of Challenge
-		output := forkIOVariable(challenge.OutputVariable)
-		input := forkIOVariables(challenge.InputVariables)
-
-		// [STEP 3] Create new Challenge with forked IO Variables
-		forkedChallenge, err := factory.NewChallenge(
-			challenge.Title,
-			challenge.Description,
-			challenge.Tags,
-			challenge.Status,
-			challenge.Difficulty,
-			challenge.WorkerTimeLimit,
-			challenge.WorkerMemoryLimit,
-			input,
-			output,
-			challenge.Constraints,
-			userID,
-		)
-
-		if err != nil {
-			return nil, fmt.Errorf("failed to create forked challenge: %w", err)
-		}
-
-		// [STEP 4] Fork TestCases and associate them with the new Challenge
-		forkedTestCases, err := forkTestCases(testCases, forkedChallenge.ID)
-		if err != nil {
-			return nil, fmt.Errorf("failed to fork test cases: %w", err)
-		}
-
-		// [STEP 5] Save forked Challenge and TestCases in repository
-		new_challenge, err := domain_services.CreateChallenge(ctx, forkedChallenge, challengeRepository, ioVariableRepository)
-		if err != nil {
-			return nil, fmt.Errorf("failed to save forked challenge: %w", err)
-		}
-
-		// [STEP 6] Save forked TestCases in repository
-		for _, tc := range forkedTestCases {
-			_, err = domain_services.CreateTestCase(ctx, &tc, testCaseRepository, ioVariableRepository)
-			if err != nil {
-				return nil, fmt.Errorf("failed to save forked test case: %w", err)
-			}
-		}
-
-		return new_challenge, nil
+	// [STEP 1] Get Challenge TestCases
+	testCases, err := testCaseRepository.GetTestCasesByChallengeID(ctx, challenge.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get test cases for challenge: %w", err)
 	}
 
-func forkTestCase (testCase *Entities.TestCase,
+	// [STEP 2] Fork IO Variables of Challenge
+	output := forkIOVariable(challenge.OutputVariable)
+	input := forkIOVariables(challenge.InputVariables)
+
+	// [STEP 3] Create new Challenge with forked IO Variables
+	forkedChallenge, err := factory.NewChallenge(
+		challenge.Title,
+		challenge.Description,
+		challenge.Tags,
+		challenge.Status,
+		challenge.Difficulty,
+		challenge.WorkerTimeLimit,
+		challenge.WorkerMemoryLimit,
+		input,
+		output,
+		challenge.Constraints,
+		userID,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create forked challenge: %w", err)
+	}
+
+	// [STEP 4] Fork TestCases and associate them with the new Challenge
+	forkedTestCases, err := forkTestCases(testCases, forkedChallenge.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fork test cases: %w", err)
+	}
+
+	// [STEP 5] Save forked Challenge and TestCases in repository
+	new_challenge, err := domain_services.CreateChallenge(ctx, forkedChallenge, challengeRepository, ioVariableRepository)
+	if err != nil {
+		return nil, fmt.Errorf("failed to save forked challenge: %w", err)
+	}
+
+	// [STEP 6] Save forked TestCases in repository
+	for _, tc := range forkedTestCases {
+		_, err = domain_services.CreateTestCase(ctx, &tc, testCaseRepository, ioVariableRepository)
+		if err != nil {
+			return nil, fmt.Errorf("failed to save forked test case: %w", err)
+		}
+	}
+
+	return new_challenge, nil
+}
+
+func forkTestCase(testCase *Entities.TestCase,
 	newChallengeID string,
 ) (*Entities.TestCase, error) {
 	if testCase == nil {
@@ -108,10 +106,10 @@ func forkTestCases(testCases []*Entities.TestCase,
 	return forkedTestCases, nil
 }
 
-func forkIOVariable (ioVar Entities.IOVariable) Entities.IOVariable {
+func forkIOVariable(ioVar Entities.IOVariable) Entities.IOVariable {
 	return Entities.IOVariable{
-		Name: ioVar.Name,
-		Type: ioVar.Type,
+		Name:  ioVar.Name,
+		Type:  ioVar.Type,
 		Value: ioVar.Value,
 	}
 }

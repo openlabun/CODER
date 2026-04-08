f9d0ed2035e8d1ae2d7014bc6f738a692c2cde42
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-04-08 14:02:21 -0500

    FIXED: endpoint for closing an exam didn't point from application container. Now it uses application container for executing it

--- apps/api_v2/internal/application/container/application.go
+++ apps/api_v2/internal/application/container/application.go
@@ -85,6 +85,7 @@ type SessionUseCases struct {
 type ExamUseCases struct {
 	CreateExam       *exam_crud_usecases.CreateExamUseCase
 	UpdateExam       *exam_crud_usecases.UpdateExamUseCase
+	CloseExam        *exam_crud_usecases.CloseExamUseCase
 	DeleteExam       *exam_crud_usecases.DeleteExamUseCase
 	GetExamDetails   *exam_crud_usecases.GetExamDetailsUseCase
 	GetExamsByCourse *exam_crud_usecases.GetExamsByCourseUseCase
@@ -191,6 +192,7 @@ func NewApplication(deps ApplicationDependencies) (*Application, error) {
 	app.ExamModule = ExamUseCases{
 		CreateExam:       exam_crud_usecases.NewCreateExamUseCase(deps.UserRepository, deps.CourseRepository, deps.ExamRepository),
 		UpdateExam:       exam_crud_usecases.NewUpdateExamUseCase(deps.UserRepository, deps.ExamRepository),
+		CloseExam:        exam_crud_usecases.NewCloseExamUseCase(deps.UserRepository, deps.ExamRepository),
 		DeleteExam:       exam_crud_usecases.NewDeleteExamUseCase(deps.UserRepository, deps.ExamRepository, deps.ExamItemRepository),
 		GetExamDetails:   exam_crud_usecases.NewGetExamDetailsUseCase(deps.UserRepository, deps.ExamRepository, deps.CourseRepository),
 		GetExamsByCourse: exam_crud_usecases.NewGetExamsByCourseUseCase(deps.UserRepository, deps.ExamRepository, deps.CourseRepository),

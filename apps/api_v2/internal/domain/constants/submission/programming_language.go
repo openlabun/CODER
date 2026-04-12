5ade9b17a0c6364f6d1e2ded8e89fefe347ac2c7
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-04-11 19:49:43 -0500

    REFACTOR: added new submission package for constant variables

---
+++ apps/api_v2/internal/domain/constants/submission/programming_language.go
@@ -0,0 +1,9 @@
+package constants
+
+type ProgrammingLanguage string
+
+const (
+	LanguagePython ProgrammingLanguage = "python"
+	LanguageCPP    ProgrammingLanguage = "cpp"
+	LanguageJava   ProgrammingLanguage = "java"
+)

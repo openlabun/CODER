487d32fe4e4e99d330301627932a34751d6c4444
Sebastian Maldonado <sebastianmaldonado@uninorte.edu.co>
2026-04-14 12:50:01 -0500

    FEATURE: create submission usecase now connects Submission with related ExamItemScore

--- apps/api_v2/internal/application/usecases/submission/create_submission.go
+++ apps/api_v2/internal/application/usecases/submission/create_submission.go
@@ -26,23 +26,29 @@ type CreateSubmissionUseCase struct {
 	submissionRepository submissionRepository.SubmissionRepository
 	sessionRepository    submissionRepository.SessionRepository
 	examRepository    	 examRepository.ExamRepository
+	examScoreRepository examRepository.ExamScoreRepository
 	challengeRepository  examRepository.ChallengeRepository
 	testCaseRepository   examRepository.TestCaseRepository
 	resultRepository     submissionRepository.SubmissionResultRepository
 	ioVariableRepository examRepository.IOVariableRepository
+	examItemRepository 	   examRepository.ExamItemRepository
+	examItemScoreRepository examRepository.ExamItemScoreRepository
 	publisherPort        submissionPorts.SubmissionPublisherPort
 }
 
-func NewCreateSubmissionUseCase(userRepository userRepository.UserRepository, submissionRepository submissionRepository.SubmissionRepository, sessionRepository submissionRepository.SessionRepository, examRepository examRepository.ExamRepository, challengeRepository examRepository.ChallengeRepository, testCaseRepository examRepository.TestCaseRepository, resultRepository submissionRepository.SubmissionResultRepository, ioVariableRepository examRepository.IOVariableRepository, publisherPort submissionPorts.SubmissionPublisherPort) *CreateSubmissionUseCase {
+func NewCreateSubmissionUseCase(userRepository userRepository.UserRepository, submissionRepository submissionRepository.SubmissionRepository, sessionRepository submissionRepository.SessionRepository, examRepository examRepository.ExamRepository, examScoreRepository examRepository.ExamScoreRepository, challengeRepository examRepository.ChallengeRepository, testCaseRepository examRepository.TestCaseRepository, resultRepository submissionRepository.SubmissionResultRepository, ioVariableRepository examRepository.IOVariableRepository, examItemRepository examRepository.ExamItemRepository, examItemScoreRepository examRepository.ExamItemScoreRepository, publisherPort submissionPorts.SubmissionPublisherPort) *CreateSubmissionUseCase {
 	return &CreateSubmissionUseCase{
 		userRepository:       userRepository,
 		submissionRepository: submissionRepository,
 		sessionRepository:    sessionRepository,
 		examRepository:    	  examRepository,
+		examScoreRepository:  examScoreRepository,
 		challengeRepository:  challengeRepository,
 		testCaseRepository:   testCaseRepository,
 		resultRepository:     resultRepository,
 		ioVariableRepository: ioVariableRepository,
+		examItemRepository:   examItemRepository,
+		examItemScoreRepository: examItemScoreRepository,
 		publisherPort:        publisherPort,
 	}
 }
@@ -105,19 +111,51 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 		return nil, fmt.Errorf("session with id %q is not active", input.SessionID)
 	}
 
-	// [STEP 7] Create submission with user provided values
-	submission, err := mapper.MapCreateSubmissionInputToSubmissionEntity(user.ID, input)
+	// [STEP 7] Get exam item score for this submission
+	// [STEP 7.1] Get ExamItem
+	examItems, err := uc.examItemRepository.GetExamItem(ctx, &session.ExamID, &input.ChallengeID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get exam item: %w", err)
+	}
+
+	if len(examItems) == 0 {
+		return nil, fmt.Errorf("no exam items found for exam %q and challenge %q", session.ExamID, input.ChallengeID)
+	}
+
+	// [STEP 7.2] Get ExamScore
+	examScore, err := uc.examScoreRepository.GetExamScoreBySessionID(ctx, session.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get exam score: %w", err)
+	}
+
+	if examScore == nil {
+		return nil, fmt.Errorf("no exam score found for session %q", session.ID)
+	}
+
+	// [STEP 7.3] Get ExamItemScore for the exam item of the submission
+	examItem := examItems[0]
+	examItemScore, err := uc.examItemScoreRepository.GetExamItemScore(ctx, examItem.ID , examScore.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get exam item score: %w", err)
+	}
+
+	if examItemScore == nil {
+		return nil, fmt.Errorf("no exam item score found for exam item %q and student %q", examItem.ID, user.Username)
+	}
+
+	// [STEP 8] Create submission with user provided values
+	submission, err := mapper.MapCreateSubmissionInputToSubmissionEntity(user.ID, examItemScore, input)
 	if err != nil {
 		return nil, err
 	}
 
-	// [STEP 8] Save submission in database
+	// [STEP 9] Save submission in database
 	createdSubmission, err := uc.submissionRepository.CreateSubmission(ctx, submission)
 	if err != nil {
 		return nil, err
 	}
 
-	// [STEP 9] Get challenge and validate language is allowed
+	// [STEP 10] Get challenge and validate language is allowed
 	challenge, err := uc.challengeRepository.GetChallengeByID(ctx, input.ChallengeID)
 	if err != nil {
 		return nil, err
@@ -131,7 +169,7 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 		return nil, fmt.Errorf("language %q is not allowed for challenge with id %q", submission.Language, input.ChallengeID)
 	}
 
-	// [STEP 10] Get test cases of the challenge
+	// [STEP 11] Get test cases of the challenge
 	testCases, err := uc.testCaseRepository.GetTestCasesByChallengeID(ctx, input.ChallengeID)
 	if err != nil {
 		return nil, err
@@ -141,7 +179,7 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 		return nil, fmt.Errorf("no test cases found for challenge with id %q", input.ChallengeID)
 	}
 
-	// [STEP 11] Create submission results for each test case of the challenge
+	// [STEP 12] Create submission results for each test case of the challenge
 	var publishedResults []dtos.SubmissionResultPublishedDTO
 	for _, testCase := range testCases {
 		result, err := uc.createSubmissionResultsForTestCases(ctx, createdSubmission.ID, *testCase)
@@ -150,7 +188,7 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 		}
 
 		if result != nil {
-			// [STEP 12] Create DTO for publishing
+			// [STEP 13] Create DTO for publishing
 			publishedResult := mapper.MapSubmissionResultToPublishedDTO(*createdSubmission, *result, *testCase, *challenge)
 			if publishedResult != nil {
 				publishedResults = append(publishedResults, *publishedResult)
@@ -158,7 +196,7 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 		}
 	}
 
-	// [STEP 13] Publish submission created event to message broker for asynchronous processing of submission results
+	// [STEP 14] Publish submission created event to message broker for asynchronous processing of submission results
 	for _, publishedResult := range publishedResults {
 		err = uc.publisherPort.PublishSubmission(publishedResult)
 		if err != nil {
@@ -170,18 +208,18 @@ func (uc *CreateSubmissionUseCase) Execute(ctx context.Context, input dtos.Creat
 }
 
 func (uc *CreateSubmissionUseCase) createSubmissionResultsForTestCases(ctx context.Context, submissionID string, testCase examEntities.TestCase) (*Entities.SubmissionResult, error) {
-	// [STEP 11.1] If TestCase is custom, discard
+	// [STEP 12.1] If TestCase is custom, discard
 	if testCase.Custom {
 		return nil, nil
 	}
 	
-	// [STEP 11.2] Create submission result entity with user provided values
+	// [STEP 12.2] Create submission result entity with user provided values
 	submissionResult, err := mapper.MapSubmissionResultEntity(submissionID, testCase.ID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to map submission result entity: %w", err)
 	}
 
-	// [STEP 11.3] Save submission result in database
+	// [STEP 12.3] Save submission result in database
 	result, err := domain_services.CreateSubmissionResult(ctx, submissionResult, uc.resultRepository, uc.ioVariableRepository)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create submission result: %w", err)
